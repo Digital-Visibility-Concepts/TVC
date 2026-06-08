2fc3de4483e8743efa923560ff4a56219b560d94
Sarthak Keche <142013428+Sarthakkeche@users.noreply.github.com>
2026-06-08 22:13:38 +0530

    Add VeeraChat component to Appapp-chatbox

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { Outlet, ScrollRestoration } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import VeeraChat from "./components/VeeraChat";
+
 
 export default function App() {
   return (
     <>
       <ScrollRestoration />
+      <VeeraChat />
       <Navbar />
       <Outlet />
       <Footer />
